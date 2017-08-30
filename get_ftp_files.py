c33f0d6fcb5a0edb928a7c6c08af4f65e10a1e27
Rachel Haggerty <rhaggerty@rhaggerty.local>
2017-08-29 20:39:33 -0500

    cleaning and summary things

--- get_ftp_files.py
+++ get_ftp_files.py
@@ -1,28 +1,43 @@
 #!/usr/bin/env python
-# The naming convention for individual documents is: <bill chamber><bill type><bill suffix><bill version>
-# # File Name   Description
-# # <bill chamber>  H=House, S=Senate
-# # <bill type> B=Bill, J=Joint Resolution, R=Resolution, C=Concurrent Resolution
-# # <bill suffix>   5-digit number
-# # <bill version>  I=Introduced, S=Senate Committee Report, H=House Committee Report, E=Engrossed, F=Enrolled
-#
-ftp = FTP('ftp.legis.state.tx.us')
-ftp.login()
+"""
+This script pulls 100 State of Texas House Bill HTML files of the 851 
+legislative session from state.tx.us FTP server and saves them locally 
+as individual .htm files.
 
-#Directory structure: /bills/<legislative session>/<document type>/<text format>/<bill type>/<grouping of 100 bills>
-ftp.cwd('/bills/851/billtext/html/house_bills/HB00001_HB00099')
-files = ftp.nlst()
+Usage: ./get_ftp_files.py <group name> #group name like HB00001_HB00099
 
-local_dir = '~/remote/bills/tx/data/html'
+The naming convention for individual documents is: <bill chamber><bill type><bill suffix><bill version>
+File Name   Description
+<bill chamber>  H=House, S=Senate
+<bill type> B=Bill, J=Joint Resolution, R=Resolution, C=Concurrent Resolution
+<bill suffix>   5-digit number
+<bill version>  I=Introduced, S=Senate Committee Report, H=House Committee Report, E=Engrossed, F=Enrolled
+"""
 
-for file in files:
-    try:
-        local_filename = os.path.join(local_dir, file)
-        print "Getting filename " + file
-        ftp.retrbinary('RETR %s' % file, open(local_filename, 'wb').write)
-        print "Saving at %s" % local_filename
-    except Exception, err:
-        print err
-#         if (is_empty(local_filename)):
-#             os.remove(local_filename)
-        continue
+from __future__ import print_function
+
+import os
+import sys
+
+from ftplib import FTP
+
+
+def get_files():
+    ftp = FTP('ftp.legis.state.tx.us')
+    ftp.login()
+    #Directory structure: /bills/<legislative session>/<document type>/<text format>/<bill type>/<grouping of 100 bills>
+    #TODO: possibly make legislative session a command line argument
+    ftp.cwd('/bills/851/billtext/html/house_bills/{}'.format(sys.argv[1]))
+    files = ftp.nlst()
+    html_dir = './tx/data/html'
+    if not os.path.exists(html_dir):
+            os.makedirs(html_dir)
+    
+    for file in files:
+        html_filename = os.path.join(html_dir, file)
+        print("Getting filename " + file)
+        ftp.retrbinary('RETR {}'.format(file), open(html_filename, 'wb').write)
+        print("Saving at {}".format(html_filename))
+
+if __name__ == '__main__':
+    get_files()
