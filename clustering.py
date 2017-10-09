7817cc76b739292e023c92aeefb167bfd577afd0
Rachel Haggerty <rhaggerty@rhaggerty.local>
2017-10-09 18:08:11 -0500

    cleaned up

--- clustering.py
+++ clustering.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import glob
 import os
 import re
@@ -22,7 +24,8 @@ class Clustering(object):
         self.text_clean = tc.TextPrep(local_dir)
         self.num_clusters = num_clusters
         self.tfidf_matrix, self.terms = self.vectorize()
-    
+        self.clusters, self.top_terms_dict = self.cluster()
+
     def vectorize(self):
         tfidf_vectorizer = TfidfVectorizer(max_df=.8,
                                            stop_words='english',
@@ -35,56 +38,39 @@ class Clustering(object):
         terms = tfidf_vectorizer.get_feature_names()
         return tfidf_matrix, terms
 
-    
     def cluster(self):
         num_clusters = self.num_clusters
         km = KMeans(n_clusters=num_clusters)
         km.fit(self.tfidf_matrix)
         clusters = km.labels_.tolist()
-        bills = {'names': self.text_clean.bill_name_list,
-                 'synopsis': self.text_clean.summary_list,
-                 'cluster': clusters
-                }
-        bills_df = pd.DataFrame(bills,
-                                index = [clusters], 
-                                columns = ['synopsis', 'cluster', 'names']
-                               )
         vocab_df = self.text_clean.build_vocab_df()
-        print("Top terms per cluster:")
         order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
-        for cluster in range(num_clusters):
-            print("Cluster {}:".format(cluster + 1))
-
-            for ind in order_centroids[cluster, :10]: #number of terms
+        top_terms_dict = {}
+        for cluster in range(self.num_clusters):
+            top_terms_list = []
+            for ind in order_centroids[cluster, :10]: #replace 10 with n words per cluster
                 top_terms = vocab_df.loc[self.terms[ind].split(' ')].values.tolist()[0][0]
-                print(' {}'.format(top_terms), end=',')
-            print()
-            
-        return clusters
-            
+                top_terms_list.append(top_terms)
+            top_terms_dict[cluster] = top_terms_list    
+        return clusters, top_terms_dict
+           
     def plot(self):
         dist = 1 - cosine_similarity(self.tfidf_matrix)
-        mds = MDS(n_components=2, dissimilarity="precomputed", random_state=1)
+        mds = MDS(n_components=2, dissimilarity='precomputed', random_state=1)
         pos = mds.fit_transform(dist)
         xs, ys = pos[:, 0], pos[:, 1]
-        cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
-        cluster_names = {0: 'school, public, finance, teacher', 
-                         1: 'tax, ad, valorem, appraised, homestead', 
-                         2: 'certain, criminal, state, offense', 
-                         3: 'health, abortion, certain, physicians', 
-        }
-
+        color_list = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e']
+        cluster_colors = dict(zip(self.top_terms_dict.keys(), color_list))
+        cluster_names = self.top_terms_dict
         cluster_df = pd.DataFrame(dict(x=xs,
                                        y=ys,
-                                       label=self.cluster(),
-                                       title=self.text_clean.bill_name_list())
-                                       )
+                                       label=self.clusters,
+                                       title=self.text_clean.bill_name_list()))
         groups = cluster_df.groupby('label')
         fig, ax = plt.subplots(figsize=(17, 9))
         ax.margins(0.05)
-
         for name, group in groups:
-            ax.plot(group.x, group.y, marker='o', linestyle='', ms=12, 
+            ax.plot(group.x, group.y, marker='o', linestyle='', ms=10, 
                     label=cluster_names[name], color=cluster_colors[name], 
                     mec='none')
             ax.set_aspect('auto')
@@ -94,10 +80,12 @@ class Clustering(object):
             ax.tick_params(axis= 'y', 
                            left='off',
                            labelleft='off')
-
-        ax.legend(numpoints=1)
-
+        ax.legend(numpoints=1, loc='lower left', bbox_to_anchor=(0, -0.1))
         for i in range(len(cluster_df)):
             ax.text(cluster_df.loc[i]['x'], cluster_df.loc[i]['y'], cluster_df.loc[i]['title'], size=8)  
-
         plt.show()
+
+
+if __name__ == '__main__':
+    doit = Clustering('/Users/rhaggerty/remote/bills/tx/data/', 4)
+    doit.plot()
